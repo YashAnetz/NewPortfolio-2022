5ec514ee1b73289e2c0d9639c65c1a5cb7da9410
YashAnetz <55511290+YashAnetz@users.noreply.github.com>
2022-07-29 02:50:20 +0530

    Update mainjs.js

--- mainjs.js
+++ mainjs.js
@@ -9,6 +9,22 @@ let frontscreen = document.querySelector(".showcase");
 let projectbutton = document.querySelector(".topro");
 let loading = document.querySelector(".loading");
 
+//Buttons
+let allb = document.querySelector(".al");
+let webb = document.querySelector(".we");
+let jsb = document.querySelector(".js");
+let androidb = document.querySelector(".an");
+let pyb = document.querySelector(".py");
+let gameb = document.querySelector(".ga");
+
+//Screens
+let alls = document.querySelector(".all");
+let webs = document.querySelector(".web");
+let jsbs = document.querySelector(".javascript");
+let androids = document.querySelector(".android");
+let pys = document.querySelector(".python");
+let games = document.querySelector(".gaming");
+
 menuButton.addEventListener("click", function () {
   menu.classList.toggle("active");
   menuButton.classList.toggle("active");
@@ -27,3 +43,63 @@ function showFunction() {
   skillsScreen.classList.add("AfterScroll");
   skillsScreen.classList.remove("BeforeScroll");
 }
+
+allb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.remove("hide");
+  webs.classList.add("hide");
+  jsbs.classList.add("hide");
+  androids.classList.add("hide");
+  games.classList.add("hide");
+  pys.classList.add("hide");
+});
+
+webb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.add("hide");
+  webs.classList.remove("hide");
+  jsbs.classList.add("hide");
+  androids.classList.add("hide");
+  games.classList.add("hide");
+  pys.classList.add("hide");
+});
+
+jsb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.add("hide");
+  webs.classList.add("hide");
+  jsbs.classList.remove("hide");
+  androids.classList.add("hide");
+  games.classList.add("hide");
+  pys.classList.add("hide");
+});
+
+androidb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.add("hide");
+  webs.classList.add("hide");
+  jsbs.classList.add("hide");
+  androids.classList.remove("hide");
+  games.classList.add("hide");
+  pys.classList.add("hide");
+});
+
+pyb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.add("hide");
+  webs.classList.add("hide");
+  jsbs.classList.add("hide");
+  androids.classList.add("hide");
+  games.classList.add("hide");
+  pys.classList.remove("hide");
+});
+
+gameb.addEventListener("click", function () {
+  console.log("hi");
+  alls.classList.add("hide");
+  webs.classList.add("hide");
+  jsbs.classList.add("hide");
+  androids.classList.add("hide");
+  games.classList.remove("hide");
+  pys.classList.add("hide");
+});
